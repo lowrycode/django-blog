4465a3ee179bdc4b8f386b3126b3b0dd3b03de64
Pete Lowry <peter.m.lowry@gmail.com>
2025-03-24 10:57:38 +0000

    Add view, template and urls for about app

--- about/views.py
+++ about/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import About
+
 
 # Create your views here.
+def about_detail(request):
+    """
+    Display an individual :model:`about.About`.
+
+    **Context**
+
+    ``about``
+        An instance of :model:`about.About`.
+
+    **Template:**
+
+    :template:`about/about.html`
+    """
+
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
